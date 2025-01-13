19487ad64f271f25f5483d02aa2838a98c3c74a7
Siddharth Chaturvedi <50156704+i-m-iron-man@users.noreply.github.com>
2025-01-13 16:06:37 +0100

    Update benchmark_wolfsheep.py

--- WolfSheep/Mesa/benchmark_wolfsheep.py
+++ WolfSheep/Mesa/benchmark_wolfsheep.py
@@ -16,31 +16,59 @@ from WolfSheep import WolfSheep
 import random
 random.seed(42)
 
-def runthemodel(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time):
-    wolfsheep = WolfSheep(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time)
+seed = random.randint(1, 10000)
+height = 1000
+width = 1000
+initial_sheep = 10000
+initial_wolves = 5000
+sheep_reproduce = 0.5
+wolf_reproduce = 0.2
+grass_regrowth_time = 40
+
+wolfsheep = WolfSheep(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time)
+
+def runthemodel():
     for i in range(0, 100):
       wolfsheep.step()
-
-seed = random.randint(1, 10000)
-height = {}
-width = {}
-initial_sheep = {}
-initial_wolves = {}
-sheep_reproduce = {}
-wolf_reproduce = {}
-grass_regrowth_time = {}
 """
 
-n_run = 100
+n_run = 10
 
-tt = timeit.Timer('runthemodel(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time)', 
-                  setup=setup.format(25, 25, 60, 40, 0.2, 0.1, 20))
-a = tt.repeat(n_run, 1)
-median_time = sorted(a)[n_run // 2 + n_run % 2]
-print("Mesa WolfSheep-small (ms):", median_time*1e3)
-
-tt = timeit.Timer('runthemodel(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time)', 
-                  setup=setup.format(100, 100, 1000, 500, 0.4, 0.2, 10))
+tt = timeit.Timer('runthemodel()', setup=setup.format())
 a = tt.repeat(n_run, 1)
 median_time = sorted(a)[n_run // 2 + n_run % 2]
 print("Mesa WolfSheep-large (ms):", median_time*1e3)
+
+setup = """
+gc.enable()
+import os, sys
+sys.path.insert(0, os.path.abspath("."))
+
+from agents import Sheep, Wolf, GrassPatch
+from WolfSheep import WolfSheep
+
+import random
+random.seed(42)
+
+seed = random.randint(1, 10000)
+height = 100
+width = 100
+initial_sheep = 1000
+initial_wolves = 500
+sheep_reproduce = 0.4
+wolf_reproduce = 0.2
+grass_regrowth_time = 20
+
+wolfsheep = WolfSheep(seed, height, width, initial_sheep, initial_wolves, sheep_reproduce, wolf_reproduce, grass_regrowth_time)
+
+def runthemodel():
+    for i in range(0, 100):
+      wolfsheep.step()
+"""
+
+n_run = 10
+
+tt = timeit.Timer('runthemodel()', setup=setup.format())
+a = tt.repeat(n_run, 1)
+median_time = sorted(a)[n_run // 2 + n_run % 2]
+print("Mesa WolfSheep-small (ms):", median_time*1e3)
