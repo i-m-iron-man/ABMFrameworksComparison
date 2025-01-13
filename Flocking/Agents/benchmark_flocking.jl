e793cde7a1d5af757e5025bff33d3fa65ac0a85b
Siddharth Chaturvedi <50156704+i-m-iron-man@users.noreply.github.com>
2025-01-13 15:34:50 +0100

    Update benchmark_flocking.jl

--- Flocking/Agents/benchmark_flocking.jl
+++ Flocking/Agents/benchmark_flocking.jl
@@ -8,17 +8,25 @@ rng_seed = MersenneTwister(42)
 
 rng_model() = Xoshiro(rand(rng_seed, 1:10000))
 
-function run_model(rng, extent, n_birds, visual_distance)
-    model = flocking_model(rng, extent, n_birds, visual_distance)
-    step!(model, 100)
-end
+#function run_model(rng, extent, n_birds, visual_distance)
+#    model = flocking_model(rng, extent, n_birds, visual_distance)
+#    step!(model, 100)
+#end
 
-n_run = 100
+n_run = 10
 
-a = @benchmark run_model(rng, (100, 100), 200, 5.0) setup=(rng = rng_model()) evals=1 samples=n_run seconds=1e6
-median_time = sort(a.times)[n_run ÷ 2 + n_run % 2]
+a_small = @benchmark step!(model, 100) setup = (
+    rng = rng_model();
+    model = flocking_model(rng, (200, 200), 1000, 10.0)
+) evals=1 samples=n_run seconds=1e6
+median_time = sort(a_small.times)[n_run ÷ 2 + n_run % 2]
 println("Agents.jl Flocking-small (ms): ", median_time * 1e-6)
 
-a = @benchmark run_model(rng, (150, 150), 400, 15.0) setup=(rng = rng_model()) evals=1 samples=n_run seconds=1e6
-median_time = sort(a.times)[n_run ÷ 2 + n_run % 2]
+
+a_large = @benchmark step!(model, 100) setup = (
+    rng = rng_model();
+    model = flocking_model(rng, (500, 500), 10000, 10.0)
+) evals=1 samples=n_run seconds=1e6
+median_time = sort(a_large.times)[n_run ÷ 2 + n_run % 2]
 println("Agents.jl Flocking-large (ms): ", median_time * 1e-6)
+
