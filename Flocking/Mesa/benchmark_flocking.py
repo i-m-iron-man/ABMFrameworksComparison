0dec5b9634fa2e755c3749a54266df333fc83417
Siddharth Chaturvedi <50156704+i-m-iron-man@users.noreply.github.com>
2025-01-13 15:40:55 +0100

    Update benchmark_flocking.py

--- Flocking/Mesa/benchmark_flocking.py
+++ Flocking/Mesa/benchmark_flocking.py
@@ -1,11 +1,7 @@
-# Use Python 3
-
-# Only collect the number of wolves and sheeps per step.
-
 import timeit
 import gc
 
-setup = """
+setup_small = """
 gc.enable()
 import os, sys
 sys.path.insert(0, os.path.abspath("."))
@@ -14,27 +10,49 @@ from Flocking import BoidFlockers
 
 import random
 random.seed(42)
-
-def runthemodel(seed, population, width, height, vision):
-    flock = BoidFlockers(seed, population, width, height, vision)
-    for i in range(0, 100):
-      flock.step()
-
 seed = random.randint(1, 10000)
-population={}
-width={}
-height={}
-vision={}
+population = 1000
+width = 200
+height = 200
+vision = 10.0
+flock_small = BoidFlockers(seed, population, width, height, vision)
+
+def runthemodel_small():
+  for i in range(0, 100):
+      flock_small.step()
+
 """
 
 n_run = 100
-
-tt = timeit.Timer('runthemodel(seed, population, width, height, vision)', setup=setup.format(200, 100, 100, 5))
+tt = timeit.Timer('runthemodel_small()', setup=setup_small.format())
 a = tt.repeat(n_run, 1)
 median_time = sorted(a)[n_run // 2 + n_run % 2]
 print("Mesa Flocking-small (ms):", median_time*1e3)
 
-tt = timeit.Timer('runthemodel(seed, population, width, height, vision)', setup=setup.format(400, 150, 150, 15))
+
+setup_large = """
+gc.enable()
+import os, sys
+sys.path.insert(0, os.path.abspath("."))
+
+from Flocking import BoidFlockers
+
+import random
+random.seed(42)
+seed = random.randint(1, 10000)
+population = 10000
+width = 500
+height = 500
+vision = 10.0
+flock_large = BoidFlockers(seed, population, width, height, vision)
+
+def runthemodel_large():
+  for i in range(0, 100):
+      flock_large.step()
+
+"""
+n_run = 100
+tt = timeit.Timer('runthemodel_large()', setup=setup_large.format())
 a = tt.repeat(n_run, 1)
 median_time = sorted(a)[n_run // 2 + n_run % 2]
 print("Mesa Flocking-large (ms):", median_time*1e3)
